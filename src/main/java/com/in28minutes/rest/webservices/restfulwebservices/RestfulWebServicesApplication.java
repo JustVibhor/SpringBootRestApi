cd5c6f445359051b4ee8f39dbfa72a28de207a9a
JustVibhor <vibhorebhatneriya@gmail.com>
2023-10-25 21:59:58 +0530

    Todo Api getById and username and deleteById

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,12 +2,28 @@ package com.in28minutes.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+	// Here we're trying to enable Cross mapping b/w localhost:3000/ to locahost:8080/
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoService.java
@@ -14,11 +14,11 @@ public class TodoService {
 	private static int todosCount = 0;
 	
 	static {
-		todos.add(new Todo(++todosCount, "in28minutes","Get AWS Certified", 
+		todos.add(new Todo(++todosCount, "Vibhor","Get AWS Certified",
 							LocalDate.now().plusYears(10), false ));
-		todos.add(new Todo(++todosCount, "in28minutes","Learn DevOps", 
+		todos.add(new Todo(++todosCount, "Vibhor","Learn DevOps",
 				LocalDate.now().plusYears(11), false ));
-		todos.add(new Todo(++todosCount, "in28minutes","Learn Full Stack Development", 
+		todos.add(new Todo(++todosCount, "Vibhor","Learn Full Stack Development",
 				LocalDate.now().plusYears(12), false ));
 	}
 	
@@ -34,19 +34,20 @@ public class TodoService {
 		return todo;
 	}
 	
-	public void deleteById(int id) {
-		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+	public void deleteById(int id, String username) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id && todo.getUsername().equals(username);
 		todos.removeIf(predicate);
 	}
 
-	public Todo findById(int id) {
+	public Todo findById(int id, String username) {
+		List<Todo> listOftodos = findByUsername(username);
 		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
-		Todo todo = todos.stream().filter(predicate).findFirst().get();
+		Todo todo = listOftodos.stream().filter(predicate).findFirst().get();
 		return todo;
 	}
 
 	public void updateTodo(Todo todo) {
-		deleteById(todo.getId());
+		deleteById(todo.getId(), todo.getUsername());
 		todos.add(todo);
 	}
 }
